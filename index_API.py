2037e3b07a776da0d07379aa41e65da1c9f03c12
Ericpso <ericpso.89@gmail.com>
2021-08-28 17:01:53 -0300

    Added a cache of 5 mins for the requests

--- index_API.py
+++ index_API.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from flask_caching import Cache
 import requests
 import json
 
@@ -18,11 +19,16 @@ def city_weather_hist():
 # Get the cache data for the specified ​city_name​, otherwise fetches from the Open Weather API, caches and returns the results.
 @app.route("/weather/<city_name>")
 def city_weather(city_name=""):
+    response = cache.get(city_name)
+
+    if response:
+        return f"Fetching weather for city <b>{city_name}</b> from cache. <p>{response}</p>"
 
     # call OpenWeather API and save json response
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&APPID={app.config['API_KEY']}"
 
     response = requests.get(url).json()
+    cache.set(city_name, response) # CACHE_DEFAULT_TIMEOUT is imported from app.config
 
     return f"Fetching weather for city <b>{city_name}</b> <p>{response}</p>"
 
@@ -31,6 +37,9 @@ if __name__ == "__main__":
     app.config.update(
         API_KEY = "acd04fdc6116982e7ee72c278537afef",           # OpenWeather API_KEY
         latestCitiesNum = 5,                                    # Number of cities to be returned by default
+        CACHE_TYPE = "SimpleCache",                             # Flask-Caching related configs
+        CACHE_DEFAULT_TIMEOUT = 300                             # Cashe Timeout in seconds
     )
 
+    cache = Cache(app)
     app.run()
