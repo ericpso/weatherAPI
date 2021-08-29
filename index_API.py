a7b09f57042e001a8a331c52406a5564a5c854a5
Ericpso <ericpso.89@gmail.com>
2021-08-29 18:19:47 -0300

    Added handling  for incorrect inputs

--- index_API.py
+++ index_API.py
@@ -13,33 +13,49 @@ def home():
 # Get all the cached cities, up to the latest n entries (configurable) or ​max_number​ ​(if specified)​.
 @app.route("/weather")
 def city_weather_hist():
-
-    max_number = request.args.get('max', default=app.config['latestCitiesNum'], type=int)  # number of cities passed as argument
+    
+    max_number = request.args.get('max', default=app.config['latestCitiesNum'])  # number of cities passed as argument
     weather_hist = {}
 
-    for i in range(0,max_number):
-        weather_hist[search_history[i]] = cache.get(search_history[i])
+    try:
+        if int(max_number) < 0 :
+            raise ValueError
 
-    return f"Retuning the last {max_number} cached cities. <p>{json.dumps(weather_hist)}</p>"
+        if max_number > app.config["CACHE_SIZE"]:
+            raise IndexError
+
+        for city in search_history[0:max_number]:
+            weather_hist[city] = cache.get(city)
+
+    except ValueError:
+        return "max provided is not a positive integer", 400
+    except IndexError:
+        return "I only have memory for the last 300 cached entries, please set an ask for max<=300.", 422 
+
+    return json.dumps(weather_hist)
 
 # Get the cache data for the specified ​city_name​, otherwise fetches from the Open Weather API, caches and returns the results.
 @app.route("/weather/<city_name>")
 def city_weather(city_name=""):
-    response = cache.get(city_name)
+    response = cache.get(city_name.casefold())
 
     if response:
-        return f"Fetching weather for city <b>{city_name}</b> from cache. <p>{response}</p>"
+        return response
 
     # call OpenWeather API and save json response
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&APPID={app.config['API_KEY']}"
 
-    search_history.pop()
-    search_history.insert(0,city_name)
-
     response = requests.get(url).json()
-    cache.set(city_name, response) # CACHE_DEFAULT_TIMEOUT is imported from app.config
 
-    return f"Fetching weather for city <b>{city_name}</b> <p>{response}</p>"
+    if response["cod"] !=200:
+        return response
+
+    cache.set(city_name.casefold(), response) # CACHE_DEFAULT_TIMEOUT is imported from app.config
+
+    search_history.pop()
+    search_history.insert(0,city_name.casefold())
+
+    return response
 
 
 if __name__ == "__main__":
@@ -47,9 +63,10 @@ if __name__ == "__main__":
         API_KEY = "acd04fdc6116982e7ee72c278537afef",           # OpenWeather API_KEY
         latestCitiesNum = 5,                                    # Number of cities to be returned by default
         CACHE_TYPE = "SimpleCache",                             # Flask-Caching related configs
-        CACHE_DEFAULT_TIMEOUT = 300                             # Cashe Timeout in seconds
+        CACHE_DEFAULT_TIMEOUT = 300,                            # Cashe Timeout in seconds
+        CACHE_SIZE = 300
     )
 
-    search_history = ['']*100
+    search_history = ['']*app.config["CACHE_SIZE"]
     cache = Cache(app)
     app.run()
